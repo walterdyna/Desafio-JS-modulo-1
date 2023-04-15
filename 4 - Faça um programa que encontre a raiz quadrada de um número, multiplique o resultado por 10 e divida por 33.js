2a1a1093aab44a5eb003a84d5b7db248ca2ab491
walterdyna <alexdyna@gmail.com>
2023-04-15 17:53:23 -0300

    ...

--- 4 - Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33.js
+++ 4 - Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33.js
@@ -1,6 +1,14 @@
 //Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33
 
+let resultadoPor = 10
+let divida = 33
+
+const raizPor10e33 = 9^(1/2)
 
 const numeroDeRaiz = 9
 
-console.log(Math.sqrt(numeroDeRaiz,2))
+console.log(Math.sqrt(numeroDeRaiz,2)*10/33)
+
+// console.log((raizPor10e33) *10/33)
+
+console.log(raizPor10e33)
