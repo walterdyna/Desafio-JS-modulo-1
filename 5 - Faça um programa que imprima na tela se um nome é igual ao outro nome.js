0354196287dffeecaf25dab5e4b5506469c0adb1
walterdyna <alexdyna@gmail.com>
2023-04-14 22:34:00 -0300

    Nomes iguais e diferentes

---
+++ 5 - Faça um programa que imprima na tela se um nome é igual ao outro nome.js
@@ -0,0 +1,12 @@
+//Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Ex: João e João, imprime true. João e Maria, imprime false.
+
+let nome1 = "Alex"
+let nome2 = "Quezia"
+
+const nomeIgual = nome1==nome1
+
+const nomeDiferentes = nome1==nome2
+
+console.log(nomeIgual)
+
+console.log(nomeDiferentes)
