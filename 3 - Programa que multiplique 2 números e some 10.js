8cb77bdc97a4513aca7db2aba00a9b561a58bf97
walterdyna <alexdyna@gmail.com>
2023-04-14 21:58:21 -0300

    Soma de 2 numeros

---
+++ 3 - Programa que multiplique 2 números e some 10.js
@@ -0,0 +1,18 @@
+// Faça um programa que multiplique 2 números e o resultado adicione 10
+
+
+let number1 = 20
+let number2 = 30
+
+let multiplique = (20*30)+10
+
+const mult = (number1 * number2) + 10
+
+console.log = ((20*30) + 10)
+
+console.log = (multiplique)
+
+console.log = ((number1 * number2) + 10)
+
+console.log = (mult)
+
