1101179b14d7ad645b7269dfe4a6aa000c3b8d4d
walterdyna <alexdyna@gmail.com>
2023-04-14 23:05:34 -0300

    Nomes em Arrey

---
+++ 7 - Faça um programa que imprima na tela se um nome é igual ao outro nome digitado com Array.js
@@ -0,0 +1,26 @@
+// Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém, os dois nomes devem estar em um Array. Ex: const array = ["João", "Maria"]. João e João, imprime true. João e Maria, imprime false.
+
+let nomes = ["Alex" , "Quezia" , "Lorena"]
+
+const todosNomes = ["Amelia" , "Dudu" , "Maria Eduarda" , "Joao"]
+
+const linha = "------------------"
+
+console.log(nomes[1] == 1);
+
+console.log(linha)
+
+console.log(nomes[0] ===1)
+
+console.log(linha)
+console.log(linha)
+
+console.log(todosNomes[1]!==1)
+console.log(todosNomes[1]!==2)
+
+console.log(linha)
+console.log(linha)
+
+console.log(nomes [1] === "Quezia")
+console.log(nomes [1] === nomes[1])
+
