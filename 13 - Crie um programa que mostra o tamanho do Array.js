a4cdf0c312d0b3e86c9cdaad4af565db6b4c1b08
walterdyna <alexdyna@gmail.com>
2023-04-15 00:10:46 -0300

    Tamanho do Array

---
+++ 13 - Crie um programa que mostra o tamanho do Array.js
@@ -0,0 +1,10 @@
+//Crie um programa que mostra o tamanho do Array. Ex: const array = [0, 1] imprime: 2. const array = ["banana", "Maça", "laranja"] imprime: 3
+
+const nome = [ "alex", "quezia", "lorena"];
+const numero = [39+1, 39, 8];
+
+console.log("Tamanhodo do Array nome: " + nome.length);
+
+console.log( "--------------")
+
+console.log("Tamanhodo do Array numero: " + numero.length);
