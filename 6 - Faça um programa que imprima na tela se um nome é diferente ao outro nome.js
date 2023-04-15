fb5733722b18d27c290205866c16e7ef565cdd90
walterdyna <alexdyna@gmail.com>
2023-04-14 22:40:18 -0300

    Bolean funcao !==

---
+++ 6 - Faça um programa que imprima na tela se um nome é diferente ao outro nome.js
@@ -0,0 +1,12 @@
+// Faça um programa que imprima na tela se um nome é diferente ao outro
+
+let nome1 = "Alex"
+let nome2 = "Quezia"
+
+const nomeIgual = nome1!==nome1
+
+const nomeDiferentes = nome1!==nome2
+
+console.log(nomeIgual)
+
+console.log(nomeDiferentes)
