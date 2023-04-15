5f1a44fc42b8b25d7ae7da89f9506908b06ee3ea
walterdyna <alexdyna@gmail.com>
2023-04-14 23:49:38 -0300

    Math random

---
+++ 11 - [ ] Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100..js
@@ -0,0 +1,9 @@
+//Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100.
+
+
+
+console.log(Math.random() + 1)
+
+console.log("-------------------")
+
+console.log(Math.ceil(Math.random() * 100))
