8b152bbc954122e825df409254e85530119c6daf
walterdyna <alexdyna@gmail.com>
2023-04-14 22:14:08 -0300

    Nome e partner

---
+++ 4 - Faça um programa que inicie com dois nomes.js
@@ -0,0 +1,17 @@
+// [ ] Faça um programa que inicie com dois nomes, e o programa imprima na tela o seguinte dado: Olá, meu nome é (nome 1) e meu partner do CodeClub é (nome2)
+
+
+let nome1 = "Alex "
+let nome2 = "Quezia "
+
+let complemento1 = "Ola Meu nome e "
+let complemento2 = "e Meu partner do CodeClub e "
+
+const namesEPartner = "Ola Meu nome e Alex e meu Partner do CodeClub e Quezia"
+
+console.log("Ola Meu nome e " + nome1 + "e Meu partner do CodeClub e " + nome2)
+
+console.log(complemento1 + nome1 + complemento2 + nome2)
+
+console.log(namesEPartner)
+
