512a6904f57ea377578063492d75ed1a3f87b821
walterdyna <alexdyna@gmail.com>
2023-04-14 21:45:29 -0300

    Desafio-JavaScript

---
+++ Um programa que SOME 2 números.js
@@ -0,0 +1,23 @@
+
+// ---***um programa que SOME 2 números---***
+
+let numero1 = 20
+let numero2 = 30
+
+let somaDeNumeros = 33+33
+
+const sum = numero1 + numero2
+
+console.log(20+30)
+
+//******ou*******
+
+console.log(somaDeNumeros)
+
+//******ou*******
+
+console.log(numero1+numero2)
+
+//******ou*******
+
+console.log(sum)
