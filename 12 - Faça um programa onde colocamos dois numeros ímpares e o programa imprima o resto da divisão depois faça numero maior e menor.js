c359c2e735784c201a1815c686f8caee4962d01e
walterdyna <alexdyna@gmail.com>
2023-04-15 00:01:12 -0300

    Resto e maior e menor

---
+++ 12 - Faça um programa onde colocamos dois numeros ímpares e o programa imprima o resto da divisão depois faça numero maior e menor.js
@@ -0,0 +1,19 @@
+//Faça um programa onde colocamos dois numeros ímpares e o programa imprima o resto da divisão. Ex: 7/3 deve imprimir: 1, 25/5 deve imprimir: 0.Faça um programa onde entramos com dois números, e ele imprime se o primeiro número é maior que o segundo. Ex: 2 e 5. Imprime: false.Faça um programa onde entramos com dois números, e ele imprime se o primeiro número é menor que o segundo. Ex: 2 e 5. Imprime: true.
+
+let number1 = 7
+let number2 = 3
+
+const resto = number1 % number2
+
+const maior = number1 > number2
+
+const menor = number1 < number2
+
+
+console.log(resto)
+
+console.log("----------------")
+
+console.log(maior)
+
+console.log(menor)
