14c1049bee9ce2085e6f277cf37d1c5153135e2d
walterdyna <alexdyna@gmail.com>
2023-04-14 23:29:21 -0300

    modo comparativo de objetos

---
+++ 10 -  Faça um programa que imprima na tela se um nome é igual ao outro nome digitado - Objetos diferentes.js
@@ -0,0 +1,57 @@
+//Criei 5 Objetos, neles devem conter os dados de 5 pessoas que você conhece. Mínimo 5 dados por pessoa.
+
+const pessoa1 = {
+    nome:"Alex",
+    age:39,
+    sex:"Male",
+    height:1.75,
+    address: "Rua Nalson Barbosa n2"
+}
+
+const pessoa2 = {
+    nome:"Quezia",
+    age:39,
+    sex:"femele",
+    height:1.55,
+    address:"Rua Nalson Barbosa n2 - condominio andorias3"
+}
+
+const pessoa3 = {
+    nome:"Lorena",
+    age:8,
+    sex:"femele",
+    height:1.25,
+    address:"Rua Nalson Barbosa n2 - condominio andorias3 - bl11 - Apt101"
+}
+
+const pessoa4 = {
+    nome:"Amelia",
+    age:63,
+    sex:"femele",
+    height:1.60,
+    address:"3 º etapa"
+}
+
+const pessoa5 = {
+    nome:"Maria Eduarda",
+    age:12,
+    sex:"femele",
+    height:1.60,
+    address:"3 º etapa - Edf: Camelia"
+}
+
+const pessoa6 = {
+    nome:"João",
+    age:5,
+    sex:"Male",
+    height:1.10,
+    address:"3 º etapa - Edf: Camelia - apt 101"
+}
+
+console.log(pessoa1 == pessoa2)
+console.log(pessoa1 == pessoa1)
+
+console.log("-------------")
+
+console.log(pessoa1 !== pessoa2)
+console.log(pessoa1 !== pessoa1)
