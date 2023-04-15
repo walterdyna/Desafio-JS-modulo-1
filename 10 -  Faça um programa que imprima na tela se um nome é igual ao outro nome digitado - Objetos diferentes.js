1da8841ef31994149ed616ee707412b6aae32060
walterdyna <alexdyna@gmail.com>
2023-04-14 23:40:05 -0300

    Duas comparaçoes

--- 10 -  Faça um programa que imprima na tela se um nome é igual ao outro nome digitado - Objetos diferentes.js
+++ 10 -  Faça um programa que imprima na tela se um nome é igual ao outro nome digitado - Objetos diferentes.js
@@ -1,4 +1,4 @@
-//Criei 5 Objetos, neles devem conter os dados de 5 pessoas que você conhece. Mínimo 5 dados por pessoa.
+// Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém, os dois nomes devem estar em dois objetos separados. Ex: const object1 = {name:"João"}, object1 = {name:"Maria"}. João e João, imprime true. João e Maria, imprime false.Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém, os dois nomes devem estar em um objeto. Ex: const object1 = {firstName:"João", seccondName:"Maria"}. João e João, imprime true. João e Maria, imprime false.
 
 const pessoa1 = {
     nome:"Alex",
@@ -48,10 +48,40 @@ const pessoa6 = {
     address:"3 º etapa - Edf: Camelia - apt 101"
 }
 
+const pessoa7 = {
+    nome:"Dudu",
+    age:23,
+    sex:"Male",
+    height:1.70,
+    address:"3 º etapa - Edf: Camelia - apt 101 - b11",
+
+    nome2:"Luiza",
+    age2:23,
+    sex2:"Male",
+    height2:1.77,
+    address2:"3 º etapa - Edf: Camelia - apt 101 - b11 - so que nao"
+}
+
 console.log(pessoa1 == pessoa2)
 console.log(pessoa1 == pessoa1)
 
 console.log("-------------")
+console.log("-------------")
 
 console.log(pessoa1 !== pessoa2)
 console.log(pessoa1 !== pessoa1)
+
+console.log("-------------")
+console.log("-------------")
+
+console.log(pessoa7.nome == pessoa7.nome2)
+console.log(pessoa7.nome2 == pessoa7.nome2)
+
+console.log("-------------")
+console.log("-------------")
+
+console.log(pessoa7.nome !== pessoa7.nome2)
+console.log(pessoa7.nome2 !== pessoa7.nome2)
+
+
+
