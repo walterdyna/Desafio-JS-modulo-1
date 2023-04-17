6d8268b6a376c22fa3899f00e6c7645d32048198
walterdyna <alexdyna@gmail.com>
2023-04-17 14:47:53 -0300

    Numeros entre 10 e 100

--- 12 - Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100..js
+++ 12 - Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100..js
@@ -2,7 +2,11 @@
 
 
 
+console.log(Math.random()) // numeros aleatorios entre 0 e 1
+
 console.log(Math.random() + 1)
+
+console.log(Math.floor(Math.random()* (100 - 10) + 10)) // Floor arendondando tudo --- random gerando numero aleatorio --- 100 numero maximo --- - 10 valor minimo e + 10 voltando a 100
 
 console.log("-------------------")
 
