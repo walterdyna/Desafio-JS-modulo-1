3a2cccbd6fe2958b7a3681aeccfd47158848e8e6
walterdyna <alexdyna@gmail.com>
2023-04-17 14:20:20 -0300

    diferença de == e ===

--- 6 - Faça um programa que imprima na tela se um nome é igual ao outro nome.js
+++ 6 - Faça um programa que imprima na tela se um nome é igual ao outro nome.js
@@ -13,3 +13,6 @@ console.log(nomeDiferentes)
 
 console.log(nome1==nome1)
 console.log(nome1==nome2)
+
+// Com 2 == fazemos a comparação da descrição 5 e "5" os dois são 5--- iguais mais Diferentes.
+// com 3 === fazemos a comparação de igualdade 7 e "7" são escritos igual mais e forma diferente.
